908ac47c734596a7313caa282c7e12476ee9defc
opuopu <nurmdopu428@gmail.com>
2023-11-04 22:33:36 +0600

    added apis

--- my-project/src/components/Item/WorkTitle.jsx
+++ my-project/src/components/Item/WorkTitle.jsx
@@ -1,41 +1,32 @@
- import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
- 
-import { workSpace } from '../../features/workspaces/workSlic';
-import { useGetWorkspacesListQuery,useGetWorkspacesQuery } from '../../features/workspaces/workspaceApi';
- export default function WorkTitle() {
-   const [id,setId] = useState()
-    const {work} = useSelector(state=>state.work)
-     
-    
-      console.log(id)
-   
-      const {data} = useGetWorkspacesListQuery(id)
-        console.log(data)
-   const handleClick=(Id)=>{
-    
-    
-     setId(Id)
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-   }
-    
-   return (
-     
-     <div className='flex justify-between '>
-   {
-     work.map((wor,i) => (
-     
-      <div className='' key={i}>
-        
-         <button onClick={() => handleClick(wor._id)}> {i+1}</button>
-        {i+1}</div>
-     ))
-   }
-   
-     
-   
-    
-     </div>
-   )
- }
- 
+import { workSpace } from "../../features/workspaces/workSlic";
+import {
+  useGetWorkspacesListandTasksQuery,
+  useGetWorkspacesQuery,
+} from "../../features/workspaces/workspaceApi";
+export default function WorkTitle() {
+  const [id, setId] = useState();
+  const { work } = useSelector((state) => state.work);
+
+  console.log(id);
+
+  const { data } = useGetWorkspacesListandTasksQuery(id);
+  console.log(data);
+  const handleClick = (Id) => {
+    setId(Id);
+  };
+
+  return (
+    <div className="flex justify-between ">
+      {work.map((wor, i) => (
+        <div className="" key={i}>
+          <button onClick={() => handleClick(wor._id)}> {i + 1}</button>
+          {i + 1}
+        </div>
+      ))}
+    </div>
+  );
+}
